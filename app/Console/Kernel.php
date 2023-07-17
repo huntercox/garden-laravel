fe18b87bab217ad365dbf02db369d41d02591b18
Hunter Cox <huntersamuelcox1992@gmail.com>
2023-07-17 07:28:07 -0400

    Remove unused code that I wrote.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,40 +20,6 @@ class Kernel extends ConsoleKernel
 	{
 		$schedule->call(function () {
 
-			// Get all plants
-			// $plants = DB::select('select * from plants');
-
-
-			$dates = [];
-
-			// foreach (Plant::all() as $plant) {
-			// 	$date_planted 	= Carbon::createFromDate($plant->date_planted);
-			// 	$number_of_days = $plant->days_to_mature;
-			// 	$harvest_date 	= $date_planted->addDays($number_of_days);
-			// 	$harvest_date 	= Carbon::parse($harvest_date);
-
-			// 	$data = array(
-			// 		'user' => $plant->user_id,
-			// 		'harvest_date' => $harvest_date
-			// 	);
-
-			// 	$dates[] = $data;
-			// }
-
-			// foreach ($dates as $date) {
-			// 	$harvest_date = $date['harvest_date'];
-
-			// 	if ($harvest_date->isToday()) {
-			// 		$user_id = $date['user'];
-
-			// 		Mail::to($user_email)->send(new PlantReadyForHarvest());
-			// 	} else {
-
-			// 		$something = $date;
-			// 	}
-			// }
-
-
 			Plant::with('user')->get()->each(function (Plant $plant) {
 
 				if ($plant->date_planted->addDays($plant->days_to_mature)->isToday()) {
@@ -61,7 +27,7 @@ class Kernel extends ConsoleKernel
 					Mail::to($plant->user->email)->send(new PlantReadyForHarvest($plant));
 				}
 			});
-		})->everyMinute();
+		})->daily();
 	}
 
 	/**
